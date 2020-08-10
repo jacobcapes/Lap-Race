b2085cda16cae980589e413cc6dc12c6d7468c21
Jacob Capes <jacobcapes@mmc.school.nz>
2020-08-10 12:46:20 +1200

    Add project files.

---
+++ Missile follow/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Missile_follow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrSpaceship = new System.Windows.Forms.Timer(this.components);
+            this.lblRotationangle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tmrSpaceship
+            // 
+            this.tmrSpaceship.Enabled = true;
+            this.tmrSpaceship.Interval = 1;
+            this.tmrSpaceship.Tick += new System.EventHandler(this.tmrSpaceship_Tick);
+            // 
+            // lblRotationangle
+            // 
+            this.lblRotationangle.AutoSize = true;
+            this.lblRotationangle.Location = new System.Drawing.Point(477, 140);
+            this.lblRotationangle.Name = "lblRotationangle";
+            this.lblRotationangle.Size = new System.Drawing.Size(35, 13);
+            this.lblRotationangle.TabIndex = 0;
+            this.lblRotationangle.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRotationangle);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrSpaceship;
+        private System.Windows.Forms.Label lblRotationangle;
+    }
+}
+
---
+++ Missile follow/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Missile_follow
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics g; //declare a graphics object called g so we can draw on the Form
+        Spaceship spaceship = new Spaceship(); //create an instance of the Spaceship Class called spaceship
+        bool turnLeft, turnRight;
+        List<Missile> missiles = new List<Missile>();
+        bool up;
+        string move;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //get the graphics used to paint on the Form control
+            g = e.Graphics;
+            //Draw the spaceship
+            spaceship.drawSpaceship(g);
+            foreach (Missile m in missiles)
+            {
+                m.drawMissile(g);
+                m.moveMissile(g);
+            }
+
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+        
+
+        }
+
+        private void tmrSpaceship_Tick(object sender, EventArgs e)
+        {
+            if (turnRight)
+            {
+                spaceship.rotationAngle += 5;
+            }
+            if (turnLeft)
+            {
+                spaceship.rotationAngle -= 5;
+        }
+            if (up) // if left arrow key pressed
+            {
+                move = "up";
+                spaceship.MoveSpaceship(move);
+                spaceship.Rotateship(spaceship.rotationAngle);
+            }
+
+            Invalidate();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { turnLeft = false; }
+            if (e.KeyData == Keys.Right) { turnRight = false; }
+            if (e.KeyData == Keys.Up) { up = false; }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                missiles.Add(new Missile(spaceship.spaceRec, spaceship.rotationAngle));
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { turnLeft = true; }
+            if (e.KeyData == Keys.Right) { turnRight = true; }
+            if (e.KeyData == Keys.Up) { up = true; }
+        }
+    }
+}
